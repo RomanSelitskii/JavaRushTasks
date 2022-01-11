43c507dd04bfa8f46d994c8796ca57acef3887ee
JavaRushTasks <selitskiiroman@gmail.ru>
2022-01-11 11:51:13 +0300

    add solution Syntax task 0218

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0218/Solution.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int numberA = console.nextInt();
+        int numberB = console.nextInt();
+        int numberC = console.nextInt();
+        int number = numberA * numberB * numberC;
+        System.out.println(number);
+
 
     }
 }
