2ad74722032f281ddd42a0b9dd28c4d0f2db3cf1
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-18 10:16:38 +0300

    add solution Syntax task 0311

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0311/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0311;
+
+import java.util.Scanner;
+
+/* 
+Высокая точность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double firstDouble = scanner.nextDouble();
+        double secondDouble = scanner.nextDouble();
+
+        if (Math.abs(secondDouble - firstDouble) < 0.000001) {
+            System.out.println("числа равны");
+        } else {
+            System.out.println("числа не равны");
+        }
+    }
+}
