0d5b1d61b434637e40d6aeddc29438be047c4aef
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-24 18:26:11 +0300

    add solution Syntax task 0509

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0509/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.pro.task05.task0509;
+
+/* 
+Таблица умножения
+*/
+
+public class Solution {
+
+    public static int[][] MULTIPLICATION_TABLE = new int[10][10];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < MULTIPLICATION_TABLE.length; i++) {
+            for (int j = 0; j < MULTIPLICATION_TABLE[i].length; j++) {
+                 MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+                 int k = MULTIPLICATION_TABLE[i][j];
+                System.out.print( k + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
