149bdf91b4ac8b5f76187a3565e33c518ccd4e6c
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-25 19:44:29 +0300

    add solution Syntax task 0508

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -10,7 +10,26 @@ public class Solution {
     public static String[] strings;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner scanner = new Scanner(System.in);
+
+        strings = new String[6];
+
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = scanner.nextLine();
+        }
+        for (int i = 0; i < strings.length - 1; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i] == null) {
+                    break;
+                }
+                if (strings[i].equals(strings[j])){
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+            }
+        }
+
+
         for (int i = 0; i < strings.length; i++) {
             System.out.print(strings[i] + ", ");
         }
