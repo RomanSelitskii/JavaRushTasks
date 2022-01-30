ee914e757d53547ca3988585273eecbd2910254b
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-30 12:00:31 +0300

    add solution Syntax task 0605

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -14,11 +14,10 @@ public class Solution {
     }
 
     public static void reverseArray(int[] array) {
-        //напишите тут ваш код
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
-            array[i] = array[array.length - i - 1];
-            array[array.length - i ] = temp;
+            array[i] = array[array.length - 1 - i];
+            array[array.length - i - 1] = temp;
         }
     }
 
