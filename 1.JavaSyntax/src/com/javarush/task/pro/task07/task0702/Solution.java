48d58d83c855a1cff626c3386411cc5503f48efe
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-31 12:36:40 +0300

    add solution Syntax task 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte a = Byte.MAX_VALUE;
+    short b = Short.MAX_VALUE;
+    int c = Integer.MAX_VALUE;
+    long d = Long.MAX_VALUE;
+    float e = Float.MAX_VALUE;
+    double f = Double.MAX_VALUE;
+    boolean g = Boolean.TRUE;
+    char i = Character.MAX_VALUE;
+}
