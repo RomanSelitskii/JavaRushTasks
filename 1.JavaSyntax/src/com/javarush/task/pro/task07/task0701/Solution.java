0f4d3e041b3119281f8d7d02ca4870098500df61
RomanSelitskii <selitskiiroman@gmail.com>
2022-01-31 12:23:48 +0300

    add solution Syntax task 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    byte a = 1;
+    short b = 1;
+    int number = 20000;
+    long  number1 = 2000000;
+    float c = 2;
+    double d = 3;
+    boolean t = true;
+    char e = 65;
+}
